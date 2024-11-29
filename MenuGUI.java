52f8a59c046204d3ee6ec555dffc885febb30580
LeonLee246 <leonpaycredit@gmail.com>
2024-11-29 19:49:59 +0900

    Add files via upload

--- MenuGUI.java
+++ MenuGUI.java
@@ -4,24 +4,14 @@ import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import brickbreaker.BrickBreaker;
+import pingpong.PingPong;
 import java.awt.Color;
 import java.awt.Font;
 
 
 public class MenuGUI extends JFrame {
 	
-	private BrickBreaker brickBreaker;
-	
 	public MenuGUI() {
-		this(null);
-	}
-	
-	public MenuGUI(BrickBreaker brickBreaker) {
-		this.brickBreaker = brickBreaker;
-		InitUI(); // 초기화 매서드 호출
-	}
-	
-	private void InitUI() {
 		
 		setTitle("Game Start!"); // 창 제목 설정
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // 창 닫기 시 종료
@@ -29,7 +19,7 @@ public class MenuGUI extends JFrame {
 		setLayout(null); // 레이아웃 설정
 		
 		
-		JLabel welcomelabel = new JLabel("Welcome to the Game");
+		JLabel welcomelabel = new JLabel("Welcome to the Game!");
 		welcomelabel.setFont(new Font("Arial", Font.BOLD, 35)); // 글꼴 설정
 		welcomelabel.setForeground(Color.BLACK); // 글자 색깔 설정
 		welcomelabel.setHorizontalAlignment(SwingConstants.CENTER); // 중앙 정렬
@@ -41,10 +31,15 @@ public class MenuGUI extends JFrame {
 		birdgameButton.setBounds(100,200,150,50);
 		birdgameButton.setBackground(Color.YELLOW);
 		birdgameButton.setForeground(Color.BLACK);
+		birdgameButton.setFocusPainted(false); // 테두리지우기
+		
 		birdgameButton.addActionListener(new ActionListener() {
 			@Override
 			public void actionPerformed(ActionEvent e) {
-				//not implemented yet
+				
+				dispose();
+				birdgame.FlappyBird.main(new String[] {});
+				
 			}
 			
 		});
@@ -55,12 +50,11 @@ public class MenuGUI extends JFrame {
 		brickgameButton.setBounds(300,200,150,50);
 		brickgameButton.setBackground(Color.YELLOW);
 		brickgameButton.setForeground(Color.BLACK);
+		brickgameButton.setFocusPainted(false); // 테두리지우기
 		brickgameButton.addActionListener(new ActionListener() {
 			@Override
 			public void actionPerformed(ActionEvent e) {
-				if (brickBreaker != null) {
-					brickBreaker.dispose();
-				}
+				dispose();
 				new BrickBreaker();
 			}
 			
@@ -73,10 +67,23 @@ public class MenuGUI extends JFrame {
 		pingpongButton.setBounds(500,200,150,50);
 		pingpongButton.setBackground(Color.YELLOW);
 		pingpongButton.setForeground(Color.BLACK);
+		pingpongButton.setFocusPainted(false); // 테두리지우기
 		pingpongButton.addActionListener(new ActionListener() {
 			@Override
 			public void actionPerformed(ActionEvent e) {
-				// not implemented yet
+				dispose();
+				JFrame frame = new JFrame("Ping Pong");
+				PingPong game = new PingPong();
+				
+				frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+				frame.setResizable(false);
+				frame.add(game);
+				frame.pack();
+				frame.setLocationRelativeTo(null);
+				frame.setVisible(true);
+				
+				Thread gamethread = new Thread(game);
+				gamethread.start();
 			}
 			
 		});
@@ -88,6 +95,7 @@ public class MenuGUI extends JFrame {
 		rankingButton.setBounds(300,350,150,50);
 		rankingButton.setBackground(Color.GREEN);
 		rankingButton.setForeground(Color.BLACK);
+		rankingButton.setFocusPainted(false); // 테두리지우기
 		rankingButton.addActionListener(new ActionListener() {
 			@Override
 			public void actionPerformed(ActionEvent e) {
@@ -103,6 +111,7 @@ public class MenuGUI extends JFrame {
 		exitButton.setBounds(300,500,150,50);
 		exitButton.setBackground(Color.CYAN);
 		exitButton.setForeground(Color.BLACK);
+		exitButton.setFocusPainted(false); // 테두리지우기
 		exitButton.addActionListener(new ActionListener() {
 			@Override
 			public void actionPerformed(ActionEvent e) {
